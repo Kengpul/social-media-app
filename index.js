d443be40cadb73a1ba9938a84c0883aa4c13bc08
pulgaresking <paulpulgares24@gmail.com>
2022-03-08 20:37:55 +0800

    homepage design

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const ejsMate = require('ejs-mate')
+const path = require('path');
 const app = express();
 
-app.get('/test', (req, res) => {
-    res.send('test page');
+app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('home')
 })
 
 app.listen(3000, () => {
